62f2d1ec78a82c635028c247befec4a96a3e5282
jordanyazzie <jordanyazzie@mail.weber.edu>
2018-09-23 15:06:21 -0600

    Final project 1

--- RPS Template/app.js
+++ RPS Template/app.js
@@ -2,63 +2,175 @@ new Vue({
     el: '#app',
     data: {
         isShown: true,
-        outcomeShown: true,
         setRounds: 10,
         userWins: 0,
         cpuWins: 0,
         rounds: 0,
-        results: ''
+        results: '',
+        resultColor: '',
+        userWidth: '0%',
+        cpuWidth: '0%'
     },
     methods: {
+//If the User picks Rock. Each choice will update rounds, wins, loses, and print results
+        userRock: function(){
 
-        runGame: function(){
+            let cc = Math.random(); //random choice selected for computer
 
-            let cc = Math.random();
-
-            if (cc < 0.34) {
-                cc = "rock"; //rock
-            } else if (cc > 0.34 && cc < 0.67) {
-                cc = "paper";//paper
-            } else {
-                cc = "scissors"; //scissors
-            }
-
-            let uc = "rock";
-
-            if ((uc == "rock" && cc == "scissors") ||
-                (uc == "paper" && cc == "rock") ||
-                (uc == "scissors" && cc == "paper")) {
-                this.userWins++;
+            if (cc < 0.34) { //rock TIE
                 this.rounds++;
-                this.results = "Round: "+ this.rounds + ". You chose "+uc+" and the computer chose "+cc+" You Won!";
-            }
-            else if ((uc == "rock" && cc == "paper") ||
-                (uc == "paper" && cc == "scissors") ||
-                (uc == "scissors" && cc == "rock")) {
+                this.resultColor = 'hsl(240, 100%, 70%)';
+                this.results = "Round: "+ this.rounds + ". You chose ROCK and the computer chose ROCK It's a tie.";
+            } else if (cc > 0.34 && cc < 0.67) { //paper LOSE
                 this.rounds++;
                 this.cpuWins++;
-                this.results = "Round: "+ this.rounds + ". You chose "+uc+" and the computer chose "+cc+" You lost...";
-            }
-            else {
+                this.resultColor = 'hsl(0, 100%, 70%)';
+                this.cpuWidth = (this.cpuWins * 10) + "%";
+                this.results = "Round: "+ this.rounds + ". You chose ROCK and the computer chose PAPER. You lost...";
+            } else { //scissors WIN
+                this.userWins++;
                 this.rounds++;
-                this.results = "Round: "+ this.rounds + ". You chose "+uc+" and the computer chose "+cc+" It's a tie.";
+                this.resultColor = 'hsl(120, 100%, 70%)';
+                this.userWidth = (this.userWins * 10) + "%";
+                this.results = "Round: "+ this.rounds + ". You chose ROCK and the computer chose SCISSORS. You Won!";
             }
-            if (this.userWins == 10){
-                alert('You have have one the game!');
+    //When 10 wins are present confirm box to play again appears. Resets data if OK selected
+            if (this.userWins == 10){ //if the user wins
+                if(confirm('You have won! Click OK to play again.')){
+                    this.rounds = 0;
+                    this.cpuWins = 0;
+                    this.userWins = 0;
+                    this.resultColor = 'yellow';
+                    this.results = "You have restarted, make choice to start again";
+                    this.cpuWidth = '0%';
+                    this.userWidth = '0%';
+                } else{
+                    this.resultColor = 'black';
+                }
             }
-            else if (this.cpuWins ==10){
-                alert('The computer beat you, sorry');
+            else if (this.cpuWins ==10){ //if the computer wins
+                if(confirm('You have won! Click OK to play again.')){
+                    this.rounds = 0;
+                    this.cpuWins = 0;
+                    this.userWins = 0;
+                    this.resultColor = 'yellow';
+                    this.results = "You have restarted, make choice to start again";
+                    this.cpuWidth = '0%';
+                    this.userWidth = '0%';
+                } else{
+                    this.resultColor = 'black';
+                }
             }
-            console.log(cc,uc,this.rounds,this.cpuWins,this.userWins);
-
         },
+//If the User picks Paper. Each choice will update rounds, wins, loses, and print results
+        userPaper: function(){
 
+            let cc = Math.random(); //random choice selected for computer
+
+            if (cc < 0.34) { //rock WIN
+                this.userWins++;
+                this.rounds++;
+                this.resultColor = 'hsl(120, 100%, 70%)';
+                this.userWidth = (this.userWins * 10) + "%";
+                this.results = "Round: "+ this.rounds + ". You chose PAPER and the computer chose ROCK. You Won!";
+            } else if (cc > 0.34 && cc < 0.67) { //paper TIE
+                this.rounds++;
+                this.resultColor = 'hsl(240, 100%, 70%)'
+                this.results = "Round: "+ this.rounds + ". You chose PAPER and the computer chose PAPER. It's a tie.";
+            } else { //scissors LOSE
+                this.rounds++;
+                this.cpuWins++;
+                this.resultColor = 'hsl(0, 100%, 70%)';
+                this.cpuWidth = (this.cpuWins * 10) + "%";
+                this.results = "Round: "+ this.rounds + ". You chose PAPER and the computer chose SCISSORS. You lost...";
+            }
+    //When 10 wins are present confirm box to play again appears. Resets data if OK selected
+            if (this.userWins == 10){ //if the user wins
+                if(confirm('You have won! Click OK to play again.')){
+                    this.rounds = 0;
+                    this.cpuWins = 0;
+                    this.userWins = 0;
+                    this.resultColor = 'yellow';
+                    this.results = "You have restarted, make choice to start again";
+                    this.cpuWidth = '0%';
+                    this.userWidth = '0%';
+                } else{
+                    this.resultColor = 'black';
+                }
+            }
+            else if (this.cpuWins ==10){ //if the computer wins
+                if(confirm('You have won! Click OK to play again.')){
+                    this.rounds = 0;
+                    this.cpuWins = 0;
+                    this.userWins = 0;
+                    this.resultColor = 'yellow';
+                    this.results = "You have restarted, make choice to start again";
+                    this.cpuWidth = '0%';
+                    this.userWidth = '0%';
+                } else{
+                    this.resultColor = 'black';
+                }
+            }
+        },
+//If the User picks Scissors. Each choice will update rounds, wins, loses, and print results
+        userScissors: function(){
+
+            let cc = Math.random(); //random choice selected for computer
+
+            if (cc < 0.34) { //rock LOSE
+                this.rounds++;
+                this.cpuWins++;
+                this.resultColor = 'hsl(0, 100%, 70%)';
+                this.cpuWidth = (this.cpuWins * 10) + "%";
+                this.results = "Round: "+ this.rounds + ". You chose SCISSORS and the computer chose ROCK, You lost...";
+            } else if (cc > 0.34 && cc < 0.67) { //paper WIN
+                this.rounds++;
+                this.userWins++;
+                this.resultColor = 'hsl(120, 100%, 70%)';
+                this.userWidth = (this.userWins * 10) + "%";
+                this.results = "Round: "+ this.rounds + ". You chose SCISSORS and the computer chose PAPER. You Won!";
+            } else { //scissors TIE
+                this.rounds++;
+                this.resultColor = 'hsl(240, 100%, 70%)';
+                this.results = "Round: "+ this.rounds + ". You chose SCISSORS and the computer chose SCISSORS. It's a tie.";
+            }
+    //When 10 wins are present confirm box to play again appears. Resets data if OK selected
+            if (this.userWins == 10){ //if the user wins
+                if(confirm('You have won! Click OK to play again.')){
+                    this.rounds = 0;
+                    this.cpuWins = 0;
+                    this.userWins = 0;
+                    this.resultColor = 'yellow';
+                    this.results = "You have restarted, make choice to start again";
+                    this.cpuWidth = '0%';
+                    this.userWidth = '0%';
+                } else{
+                    this.resultColor = 'black';
+                }
+            }
+            else if (this.cpuWins ==10){ //if the computer wins
+                if(confirm('You have won! Click OK to play again.')){
+                    this.rounds = 0;
+                    this.cpuWins = 0;
+                    this.userWins = 0;
+                    this.resultColor = 'yellow';
+                    this.results = "You have restarted, make choice to start again";
+                    this.cpuWidth = '0%';
+                    this.userWidth = '0%';
+                } else{
+                    this.resultColor = 'black';
+                }
+            }
+        },
+//Game and data is restarted when user clicks restart button
         restartGame: function(){
             this.rounds = 0;
             this.cpuWins = 0;
             this.userWins = 0;
-            this.results = "You have restarted, make choice to start again"
+            this.resultColor = 'yellow';
+            this.results = "You have restarted, make choice to start again";
+            this.cpuWidth = '0%';
+            this.userWidth = '0%';
         }
-
     }
 });
